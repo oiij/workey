9cdfd92c5eed4b61433cf66af800cbb5db437542
antrioe <antrioe@gmali.com>
2025-02-08 17:28:07 +0800

    ci(deps): ci

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
